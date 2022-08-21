48dfb1b78bdfee411c52a987c886162220137aae
ckscks038038 <ckscks038038@gmail.com>
2022-08-21 22:20:35 +0800

    Finish assignment4

---
+++ Week-2/Assignment-4/index.js
@@ -0,0 +1,16 @@
+const welcomeMsg = document.querySelector(".banner")
+const callAction = document.querySelector(".main-footer span")
+const hidden = document.querySelector(".hidden")
+
+hidden.style.display = "none"
+
+console.log(hidden)
+welcomeMsg.addEventListener("click", () => {
+  welcomeMsg.innerHTML = "<h1>Have a Good Time!</h1>"
+})
+
+callAction.addEventListener("click", () => {
+  if (hidden.style.display === "none") {
+    hidden.style.display = "flex"
+  }
+})
